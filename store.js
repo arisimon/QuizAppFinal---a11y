ad3b3cf347c89f5496fc21a82a505d5c54623d6a
Ari Simon <arisimon2@gmail.com>
2018-03-12 18:29:32 -0700

    initial commit

---
+++ store.js
@@ -0,0 +1,112 @@
+const STORE = [
+  {
+      question: 'When was Everton FC founded?',
+      answers: [
+        '1888',
+        '1878',
+        '1900',
+        '1902',
+        ],
+        correctAnswer: '1878',
+        explanation: 'Everton were founded as St. Domingo FC in 1878 so that members of the congregation of St. Domingo Methodist New Connexion Chapel in Breckfield Road North, Everton could play sport year round',
+    },
+    {
+      question: 'Who are the arch-rivals of Everton FC?',
+      answers: [
+        'Arsenal FC',
+        'Manchester United FC',
+        'Crystal Palace FC',
+        'Liverpool FC',
+        ],
+        correctAnswer: 'Liverpool FC',
+        explanation: 'Everton\'s biggest rivalry is with neighbors Liverpool, against whom they contest the Merseyside derby. The rivalry stems from an internal dispute between Everton officials and the owners of Anfield, which was then Everton\'s home ground. The dispute resulted in the formation of Liverpool F.C. in 1892',
+    },
+    {
+      question: 'Where does Everton FC play their home games?',
+      answers: [
+        'Finch Farm',
+        'Anfield Park',
+        'Goodison Park',
+        'Bellefield',
+        ],
+        correctAnswer: 'Goodison Park',
+        explanation: 'Goodison Park is located in Walton, Liverpool, England. The stadium has been home to Everton since its completion in 1892 and is one of the world\'s oldest purpose-built football grounds'
+    },
+    {
+      question: 'How many English League Titles does Everton hold?',
+      answers: [
+        '9',
+        '6',
+        '4',
+        '0',
+        ],
+      correctAnswer: '9',
+      explanation: 'Everton have been champions 9 times in the following seasons: <br>1890–91, 1914–15, 1927–28, 1931–32, 1938–39, 1962–63, 1969–70, 1984–85, 1986–87',
+    },
+    {
+      question: 'What company has their name on the Everton FC jersey in the 2017-2018 season?',
+      answers: [
+        'Chang Beer',
+        'NEC',
+        'One2one',
+        'SportPesa',
+        ],
+      correctAnswer: 'SportPesa',
+      explanation: 'Commencing in the 2017–18 season, Everton\'s shirts are sponsored by SportPesa, a sports betting platform. <br>Previous sponsors include Chang Beer (2004–17), Kejian (2002–04), one2one (1997–2002), Danka (1995–97), NEC (1985–95), and Hafnia (1979–85).',
+    },
+    {
+      question: 'Who is the current manager of Everton FC?',
+      answers: [
+        'David Moyes',
+        'Harry Catterick',
+        'Sam Allardyce',
+        'Howard Kendall',
+        ],
+      correctAnswer: 'Sam Allardyce',
+      explanation: 'The club\'s current manager, Sam Allardyce, is the sixteenth permanent holder of the position since it was established in 1939',
+    },
+    {
+      question: 'How many seasons has Everton FC played in England’s top tier (Division One/Premier League)?',
+      answers: [
+      '100',
+      '88',
+      '105',
+      '114',
+      ],
+      correctAnswer: '114',
+      explanation: 'Everton hold the record for the most seasons in England\'s top tier at 114 seasons out of 118 as of 2016–17. <br>They are one of six teams to have played in every season of the Premier League since its inception in August 1992',
+    },
+    {
+      question: 'What Everton FC player has appeared most for the club?',
+      answers: [
+        'Tim Howard',
+        'Dave Watson',
+        'Leighton Baines',
+        'Neville Southall',
+      ],
+      correctAnswer: 'Neville Southall',
+      explanation: 'Neville made a club record 578 appearances in the Football League and Premier League (750 in all competitions)',
+    },
+    {
+      question: 'Who is Everton FC\'s all-time leading scorer?',
+      answers: [
+      'Roy Vernon',
+      'Graeme Sharp',
+      'Dixie Dean',
+      'Bob Latchford',
+      ],
+      correctAnswer: 'Dixie Dean',
+      explanation: 'Dixie Dean scored a club record 383 goals in all competitions. He also holds the English national record of most goals in a season with 60',
+    },
+    {
+      question: 'What is the defining feature of the Everton FC team crest?',
+      answers: [
+        'Prince Rupert\'s Tower',
+        'The Liver bird',
+        'A cannon',
+        'A moose',
+    ],
+    correctAnswer: 'Prince Rupert\'s Tower',
+    explanation: 'Known formally as the Everton Lock-Up, it is an 18th-century structure originally used as an overnight holding place where local drunks and criminals were taken by parish constables',
+    },
+];
